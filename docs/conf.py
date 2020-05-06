a6e1e302a698ccf8235ca34cb3d78c5f6bc0dc30
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-05-06 15:31:39 +0200

    Bump version: 0.0.0 → 0.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Axel Dahlberg'
 author = 'Axel Dahlberg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
