4ced3f98abeab34c05e549494b0cb547fb565c25
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-05-26 14:16:00 +0200

    Added option to have ghz function do the corrections

--- netqasm/sdk/toolbox/multi_node.py
+++ netqasm/sdk/toolbox/multi_node.py
@@ -1,14 +1,26 @@
-def create_ghz(down_epr_socket=None, up_epr_socket=None):
+from enum import Enum, auto
+
+
+class _Role(Enum):
+    start = auto()
+    middle = auto()
+    end = auto()
+
+
+def create_ghz(down_epr_socket=None, up_epr_socket=None, down_socket=None, up_socket=None, do_corrections=False):
     r"""Local protocol to create a GHZ state between mutliples nodes.
 
     EPR pairs are generated in a line and turned into a GHZ state by performing half of a Bell measurement.
     That is, CNOT and H are applied but only the control qubit is measured.
-    This measurement outcome is returned along with the qubit to be able to know what corrections might
-    need to be applied.
+    If `do_corrections=False` (default) this measurement outcome is returned along with the qubit to be able to know
+    what corrections might need to be applied.
     If the node is at the start or end of the line, the measurement outcome 0 is always returned since there
     is no measurement performed.
     The measurement outcome indicates if the next node in the line should flip its qubit to get the standard
     GHZ state: :math:`|0\rangle^{\otimes n} + |1\rangle^{\otimes n}`.
+
+    On the other hand if `do_corrections=True`, then the classical sockets `down_socket` and/or `up_socket`
+    will be used to communicate the outcomes and automatically perform the corrections.
 
     Depending on if down_epr_socket and/or up_epr_socket is specified the node,
     either takes the role of the:
@@ -26,6 +38,13 @@ def create_ghz(down_epr_socket=None, up_epr_socket=None):
         The EPRSocket to be used for receiving EPR pairs from downstream.
     up_epr_socket : :class:`.sdk.epr_socket.EPRSocket`
         The EPRSocket to be used for create EPR pairs upstream.
+    down_socket : :class:`.sdk.classical_communication.socket.Socket`
+        The classical socket to be used for sending corrections, if `do_corrections = True`.
+    up_socket : :class:`.sdk.classical_communication.socket.Socket`
+        The classical socket to be used for sending corrections, if `do_corrections = True`.
+    do_corrections : bool
+        If corrections should be applied to make the GHZ in the standard form
+        :math:`|0\rangle^{\otimes n} + |1\rangle^{\otimes n}` or not.
 
     Returns
     -------
@@ -37,18 +56,43 @@ def create_ghz(down_epr_socket=None, up_epr_socket=None):
 
     if down_epr_socket is None:
         # Start role
+        role = _Role.start
         q = up_epr_socket.create()[0]
-        return q, 0
+        m = 0
+    else:
+        q = down_epr_socket.recv()[0]
+        if up_epr_socket is None:
+            # End role
+            role = _Role.end
+            m = 0
+        else:
+            # Middle role
+            role = _Role.middle
+            q_up = up_epr_socket.create()[0]
+            # merge the states by doing half a Bell measurement
+            q.cnot(q_up)
+            m = q_up.measure()
 
-    q_down = down_epr_socket.recv()[0]
-    if up_epr_socket is None:
-        # End role
-        return q_down, 0
+    if do_corrections:
+        if role == _Role.start:
+            _assert_socket(up_socket)
+            up_socket.send(str(0))
+        else:
+            _assert_socket(down_socket)
+            corr = int(down_socket.recv())
+            if corr == 1:
+                q.X()
+            if role == _Role.middle:
+                # Flush the subroutine
+                down_epr_socket._conn.flush()
+                _assert_socket(up_socket)
+                corr = (corr + m) % 2
+                up_socket.send(str(corr))
+        m = 0
 
-    # Middle role
-    q_up = up_epr_socket.create()[0]
-    # merge the states by doing half a Bell measurement
-    q_down.cnot(q_up)
-    m = q_up.measure()
+    return q, m
 
-    return q_down, m
+
+def _assert_socket(socket):
+    if socket is None:
+        raise TypeError("A socket is needed to do corrections")
