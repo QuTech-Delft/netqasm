e40d55a17f7d8835ef7614fba2845f27bd59224f
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-09-24 11:20:45 +0200

    Moved sdk tests to here which are tested using squidasm

---
+++ netqasm/sdk/app_config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Callable, Dict, Any, Optional
+
+from netqasm.sdk.config import LogConfig
+
+
+@dataclass
+class AppConfig:
+    app_name: str
+    node_name: str
+    main_func: Callable
+    log_config: Optional[LogConfig]
+    inputs: Dict[str, Any]
+
+
+def default_app_config(app_name: str, main_func: Callable) -> AppConfig:
+    return AppConfig(
+        app_name=app_name,
+        node_name=app_name,
+        main_func=main_func,
+        log_config=None,
+        inputs={}
+    )
