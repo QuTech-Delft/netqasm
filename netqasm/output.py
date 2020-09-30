f1000439601407d3600556f7c677b01040c2a279
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-09-30 17:27:50 +0200

    Now logging both virtual and physical qubit IDs

--- netqasm/output.py
+++ netqasm/output.py
@@ -8,6 +8,7 @@ from netqasm.typing import TypedDict
 from netqasm.subroutine import Register, ArrayEntry, Address
 from netqasm.yaml_util import dump_yaml
 from netqasm.log_util import LineTracker
+from netqasm.errors import NotAllocatedError
 
 from netqasm import instructions
 from netqasm.encoding import RegisterName
@@ -18,12 +19,7 @@ def should_ignore_instr(instr):
         isinstance(instr, instructions.core.SetInstruction)
         or isinstance(instr, instructions.core.QAllocInstruction)
         or isinstance(instr, instructions.core.QFreeInstruction)
-    )
-
-
-def should_update_qubits_but_not_log(instr):
-    return (
-        isinstance(instr, instructions.core.CreateEPRInstruction)
+        or isinstance(instr, instructions.core.CreateEPRInstruction)
         or isinstance(instr, instructions.core.RecvEPRInstruction)
     )
 
@@ -49,7 +45,8 @@ class InstrField(Enum):
     HFL = "HFL"  # Host file
     INS = "INS"  # Instruction
     OPR = "OPR"  # Operands (register, array-entries..)
-    QID = "QID"  # Qubit IDs of qubits part of operation
+    QID = "QID"  # Physical qubit IDs of qubits part of operation
+    VID = "VID"  # Virtual qubit IDs of qubits part of operation
     QST = "QST"  # Qubit states if the qubits part of the operations after execution
     OUT = "OUT"  # Measurement outcome
     QGR = "QGR"  # Dictionary specifying groups of qubit across the network
@@ -124,8 +121,6 @@ class InstrLogger(StructuredLogger):
 
     def _construct_entry(self, *args, **kwargs):
         command = kwargs['command']
-        if should_ignore_instr(command):
-            return None
         subroutine_id = kwargs['subroutine_id']
         output = kwargs['output']
         wall_time = str(datetime.now())
@@ -136,21 +131,21 @@ class InstrLogger(StructuredLogger):
         op_values = self._get_op_values(subroutine_id=subroutine_id, operands=operands)
         ops_str = [f"{op}={opv}" for op, opv in zip(operands, op_values)]
         log = f"Doing instruction {instr_name} with operands {ops_str}"
-        qubit_ids = self._get_qubit_ids(
+        virtual_qubit_ids, physical_qubit_ids = self._get_qubit_ids(
             subroutine_id=subroutine_id,
             command=command,
         )
         self._update_qubits(
             subroutine_id=subroutine_id,
             instr=command,
-            qubit_ids=qubit_ids,
+            qubit_ids=virtual_qubit_ids,
         )
-        if should_update_qubits_but_not_log(command):
+        if should_ignore_instr(command):
             return None
-        if len(qubit_ids) > 0:
+        if len(virtual_qubit_ids) > 0:
             qubit_states = self._get_qubit_states(
                 subroutine_id=subroutine_id,
-                qubit_ids=qubit_ids,
+                qubit_ids=virtual_qubit_ids,
             )
             qubit_groups = self._get_qubit_groups()
         else:
@@ -168,7 +163,8 @@ class InstrLogger(StructuredLogger):
             InstrField.HLN.value: None,
             InstrField.INS.value: instr_name,
             InstrField.OPR.value: ops_str,
-            InstrField.QID.value: qubit_ids,
+            InstrField.QID.value: virtual_qubit_ids,
+            InstrField.VID.value: physical_qubit_ids,
             InstrField.QST.value: qubit_states,
             InstrField.OUT.value: outcome,
             InstrField.QGR.value: qubit_groups,
@@ -186,6 +182,7 @@ class InstrLogger(StructuredLogger):
             instructions.core.CreateEPRInstruction,
             instructions.core.RecvEPRInstruction,
         ]
+        app_id = self._executioner._get_app_id(subroutine_id=subroutine_id)
         if any(isinstance(command, cmd_cls) for cmd_cls in epr_instructions):
             # Ignore a constant register since this indicates it's a measure directly request
             if command.qubit_addr_array.name == RegisterName.C:  # type: ignore
@@ -194,19 +191,71 @@ class InstrLogger(StructuredLogger):
                 subroutine_id=subroutine_id,
                 operand=command.qubit_addr_array,  # type: ignore
             ))
-            app_id = self._executioner._get_app_id(subroutine_id=subroutine_id)
-            return self._executioner._get_array(app_id=app_id, address=qubit_id_array_address)  # type: ignore
+            virtual_qubit_ids = self._executioner._get_array(
+                app_id=app_id,
+                address=qubit_id_array_address,
+            )  # type: ignore
 
         # Otherwise just get the qubits from the operands which have name Q
-        qubit_ids = []
+        virtual_qubit_ids = []
         for operand in command.operands:
             if isinstance(operand, Register) and operand.name == RegisterName.Q:
-                qubit_id = self._get_op_value(
+                virtual_qubit_id = self._get_op_value(
                     subroutine_id=subroutine_id,
                     operand=operand,
                 )
-                qubit_ids.append(qubit_id)
-        return qubit_ids
+                virtual_qubit_ids.append(virtual_qubit_id)
+
+        # Lookup physical qubit IDs
+        physical_qubit_ids = self._get_physical_qubit_ids(
+            app_id=app_id,
+            virtual_qubit_ids=virtual_qubit_ids,
+        )
+        return virtual_qubit_ids, physical_qubit_ids
+
+    def _get_physical_qubit_ids(self, app_id, virtual_qubit_ids):
+        physical_qubit_ids = []
+        for virtual_qubit_id in virtual_qubit_ids:
+            try:
+                physical_qubit_id = self._executioner._get_position_in_unit_module(
+                    app_id=app_id,
+                    address=virtual_qubit_id,
+                )
+            except NotAllocatedError:
+                physical_qubit_id = None
+            physical_qubit_ids.append(physical_qubit_id)
+
+        return physical_qubit_ids
+
+    def _update_qubits(
+        self,
+        subroutine_id: int,
+        instr: instructions.base.NetQASMInstruction,
+        qubit_ids: List[int],
+    ) -> None:
+        add_qubit_instrs = [
+            instructions.core.InitInstruction,
+            instructions.core.CreateEPRInstruction,
+            instructions.core.RecvEPRInstruction,
+        ]
+        remove_qubit_instrs = [
+            instructions.core.QFreeInstruction,
+        ]
+        node_name = self._get_node_name()
+        app_id = self._get_app_id(subroutine_id=subroutine_id)
+        if any(isinstance(instr, cmd_cls) for cmd_cls in add_qubit_instrs):
+            for qubit_id in qubit_ids:
+                abs_id = node_name, app_id, qubit_id
+                self.__class__._qubits.add(abs_id)
+        elif any(isinstance(instr, cmd_cls) for cmd_cls in remove_qubit_instrs):
+            for qubit_id in qubit_ids:
+                abs_id = node_name, app_id, qubit_id
+                if abs_id in self.__class__._qubits:
+                    self.__class__._qubits.remove(abs_id)
+
+    def _get_app_id(self, subroutine_id: int) -> int:
+        """Returns the app ID for a given subroutine ID"""
+        return self._executioner._get_app_id(subroutine_id=subroutine_id)
 
     @classmethod
     def _get_qubit_states(
