f5085671a9b1c0f28333fd4c90ae2b0884128ab8
Bart van der Vecht <b.vandervecht@tudelft.nl>
2020-12-17 17:34:35 +0100

    Bump version: 0.4.2 → 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Axel Dahlberg'
 author = 'Axel Dahlberg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.2'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
--- netqasm/__init__.py
+++ netqasm/__init__.py
@@ -1,5 +1,5 @@
 # Version of the package
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 
 # Version of the language
 NETQASM_VERSION = (0, 0)
