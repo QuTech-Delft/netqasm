0b1f9a6ffc9ba40f39a5d57c1d8de33308759aa9
Bart van der Vecht <b.vandervecht@tudelft.nl>
2021-02-23 18:16:55 +0100

    Prepare dist-cnot for QNE

--- netqasm/examples/apps/dist_cnot/app_controller.py
+++ netqasm/examples/apps/dist_cnot/app_controller.py
@@ -1,14 +1,20 @@
 from netqasm.sdk import EPRSocket, Qubit
 from netqasm.sdk.toolbox import set_qubit_state
 from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
+from netqasm.sdk.toolbox.sim_states import qubit_from, to_dm, get_fidelity
+from netqasm.logging.output import get_new_app_logger
 
 
 def main(app_config=None, phi=0.0, theta=0.0):
+    log_config = app_config.log_config
+
     # socket for creating an EPR pair with target
     target_epr = EPRSocket("target")
 
     # socket for communicating classical messages with target
     class_socket = Socket("controller", "target", log_config=app_config.log_config)
+
+    app_logger = get_new_app_logger(app_name="sender", log_config=log_config)
 
     # connect to the back-end
     controller = NetQASMConnection(
@@ -22,8 +28,11 @@ def main(app_config=None, phi=0.0, theta=0.0):
         epr = target_epr.create(1)[0]
 
         # initialize control qubit of the distributed CNOT
+        app_logger.log("Initializing control qubit...")
         ctrl_qubit = Qubit(controller)
         set_qubit_state(ctrl_qubit, phi, theta)
+        controller.flush()
+        app_logger.log("Initialized control qubit")
 
         # perform a local CNOT with `epr` and measure `epr`
         ctrl_qubit.cnot(epr)
@@ -39,17 +48,21 @@ def main(app_config=None, phi=0.0, theta=0.0):
         # between his EPR half and the original control qubit
         target_meas = class_socket.recv()
         if target_meas == "1":
+            app_logger.log(f"Outcome = 1, so doing Z correction")
             ctrl_qubit.Z()
+        else:
+            app_logger.log(f"Outcome = 0, no corrections needed")
 
         controller.flush()
 
         # ack the outcome
-        class_socket.send("ACK")
+        class_socket.send_silent("ACK")
 
-        # get the combined state of Controller's control and target's target
-        dm = get_qubit_state(ctrl_qubit, reduced_dm=False)
+        original_dm = to_dm(qubit_from(phi, theta))
+        final_dm = get_qubit_state(ctrl_qubit, reduced_dm=True)
 
-        return {
-            'epr_meas': int(epr_meas),
-            'final_state': dm if dm is None else dm.tolist(),
-        }
+    return {
+        'epr_meas': int(epr_meas),
+        'original_state': original_dm.tolist(),
+        'final_state': final_dm if final_dm is None else final_dm.tolist(),
+    }
--- netqasm/examples/apps/dist_cnot/app_target.py
+++ netqasm/examples/apps/dist_cnot/app_target.py
@@ -1,14 +1,20 @@
 from netqasm.sdk import EPRSocket, Qubit
 from netqasm.sdk.toolbox import set_qubit_state
-from netqasm.sdk.external import NetQASMConnection, Socket
+from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
+from netqasm.sdk.toolbox.sim_states import qubit_from, to_dm, get_fidelity
+from netqasm.logging.output import get_new_app_logger
 
 
 def main(app_config=None, phi=0.0, theta=0.0):
+    log_config = app_config.log_config
+
     # socket for creating an EPR pair with Controller
     controller_epr = EPRSocket("controller")
 
     # socket for communicating classical messages with Controller
     class_socket = Socket("target", "controller", log_config=app_config.log_config)
+
+    app_logger = get_new_app_logger(app_name="sender", log_config=log_config)
 
     # connect to the back-end
     target = NetQASMConnection(
@@ -22,25 +28,33 @@ def main(app_config=None, phi=0.0, theta=0.0):
         epr = controller_epr.recv(1)[0]
 
         # initialize target qubit of the distributed CNOT
+        app_logger.log("Initializing target qubit...")
         target_qubit = Qubit(target)
         set_qubit_state(target_qubit, phi, theta)
 
         # let back-end execute the quantum operations above
         target.flush()
+        app_logger.log("Initialized target qubit")
 
         # wait for Controller's measurement outcome
         m = class_socket.recv()
 
         # if outcome = 1, apply an X gate on the local EPR half
         if m == "1":
+            app_logger.log("Outcome = 1, so doing X correction")
             epr.X()
+        else:
+            app_logger.log("Outcome = 0, no correction needed")
 
         # At this point, `epr` is correlated with the control qubit on Controller's side.
         # (If Controller's control was in a superposition, `epr` is now entangled with it.)
         # Use `epr` as the control of a local CNOT on the target qubit.
         epr.cnot(target_qubit)
 
+        target.flush()
+
         # undo any potential entanglement between `epr` and Controller's control qubit
+        app_logger.log("Undo entanglement between control and EPR qubit")
         epr.H()
         epr_meas = epr.measure()
         target.flush()
@@ -49,8 +63,13 @@ def main(app_config=None, phi=0.0, theta=0.0):
         class_socket.send(str(epr_meas))
 
         # Wait for an ack before exiting
-        assert class_socket.recv() == "ACK"
+        assert class_socket.recv_silent() == "ACK"
+
+        original_dm = to_dm(qubit_from(phi, theta))
+        final_dm = get_qubit_state(target_qubit, reduced_dm=True)
 
     return {
-        'epr_meas': int(epr_meas)
+        'epr_meas': int(epr_meas),
+        'original_state': original_dm.tolist(),
+        'final_state': final_dm if final_dm is None else final_dm.tolist(),
     }
