41039caef117e807e7f9d4bbe09a1c8dcbab17bd
Bart van der Vecht <b.vandervecht@tudelft.nl>
2021-02-25 17:38:44 +0100

    Add qne-test-edge-cases

---
+++ netqasm/examples/apps/qne-test-edge-cases/app_alice.py
@@ -0,0 +1,42 @@
+from netqasm.sdk import EPRSocket, Qubit
+from netqasm.sdk.external import NetQASMConnection, Socket
+
+
+def main(
+    app_config=None,
+):
+    log_config = app_config.log_config
+
+    #       alice
+    # bob           charlie
+    #       david
+
+    epr_socket_bob = EPRSocket("bob")
+    epr_socket_charlie = EPRSocket("charlie")
+
+    socket = Socket("alice", "david", log_config=log_config)
+
+    alice = NetQASMConnection(
+        app_name=app_config.app_name,
+        log_config=log_config,
+        epr_sockets=[epr_socket_bob, epr_socket_charlie]
+    )
+
+    with alice:
+        epr_bob = epr_socket_bob.create(1)[0]
+        alice.flush()
+
+        epr_charlie = epr_socket_charlie.create(1)[0]
+        alice.flush()
+
+        epr_bob.cnot(epr_charlie)
+
+        q = Qubit(alice)
+        epr_bob.cnot(q)
+
+        alice.flush()
+        socket.send_silent("")
+
+
+if __name__ == "__main__":
+    main()
---
+++ netqasm/examples/apps/qne-test-edge-cases/app_charlie.py
@@ -0,0 +1,52 @@
+from netqasm.sdk import EPRSocket, Qubit
+from netqasm.sdk.external import NetQASMConnection, Socket
+
+
+def main(
+    app_config=None,
+):
+    log_config = app_config.log_config
+
+    #       alice
+    # bob           charlie
+    #       david
+
+    epr_socket_alice = EPRSocket("alice")
+    epr_socket_david = EPRSocket("david")
+
+    socket = Socket("charlie", "bob", log_config=log_config)
+
+    charlie = NetQASMConnection(
+        app_name=app_config.app_name,
+        log_config=log_config,
+        epr_sockets=[epr_socket_alice, epr_socket_david]
+    )
+
+    with charlie:
+        epr_alice = epr_socket_alice.recv(1)[0]
+        charlie.flush()
+
+        epr_david = epr_socket_david.recv(1)[0]
+        charlie.flush()
+
+        epr_alice.cnot(epr_david)
+
+        charlie.flush()
+        socket.recv_silent()
+
+        # q = Qubit(charlie)
+        # epr_alice.cnot(q)
+
+        socket.send("0123456789")
+        socket.send("0123456789-123456789")
+        socket.send("0123456789-123456789-123456789")
+        socket.send("0123456789-123456789-123456789-123456789")
+        socket.send("0123456789-123456789-123456789-123456789-123456789")
+        socket.send("0123456789-123456789-123456789-123456789-123456789-123456789")
+        socket.send("0123456789-123456789-123456789-123456789-123456789-123456789-123456789")
+
+        socket.send_silent("")
+
+
+if __name__ == "__main__":
+    main()
