86cc746cf6fe9a5fe942f0ad568c0c3100026cf9
Bart van der Vecht <b.vandervecht@tudelft.nl>
2021-03-03 15:12:39 +0100

    Add type hints to qubit.py

--- netqasm/sdk/network.py
+++ netqasm/sdk/network.py
@@ -4,28 +4,28 @@ import abc
 class NetworkInfo:
     @classmethod
     @abc.abstractmethod
-    def _get_node_id(cls, node_name) -> int:
+    def _get_node_id(cls, node_name: str) -> int:
         """Returns the node id for the node with the given name"""
         # Should be subclassed
         pass
 
     @classmethod
     @abc.abstractmethod
-    def _get_node_name(cls, node_id) -> str:
+    def _get_node_name(cls, node_id: int) -> str:
         """Returns the node name for the node with the given ID"""
         # Should be subclassed
         pass
 
     @classmethod
     @abc.abstractmethod
-    def get_node_id_for_app(cls, app_name) -> int:
+    def get_node_id_for_app(cls, app_name: str) -> int:
         """Returns the node id for the app with the given name"""
         # Should be subclassed
         pass
 
     @classmethod
     @abc.abstractmethod
-    def get_node_name_for_app(cls, app_name) -> str:
+    def get_node_name_for_app(cls, app_name: str) -> str:
         """Returns the node name for the app with the given name"""
         # Should be subclassed
         pass
