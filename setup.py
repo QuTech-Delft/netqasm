69a1d739114a818c705f1c25ac957a599795de9e
Bart van der Vecht <b.vandervecht@tudelft.nl>
2021-09-10 15:21:45 +0200

    Let setuptools install required packages

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 import setuptools
 
+with open("requirements.txt", "r") as f:
+    install_requires = [line.strip() for line in f.readlines()]
+
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(install_requires=install_requires)
