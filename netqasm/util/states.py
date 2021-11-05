6f20c4bf16f615fc3887b3121d42c9b001e0aed3
Bart van der Vecht <b.vandervecht@tudelft.nl>
2021-11-05 17:24:20 +0100

    More fixes

--- netqasm/util/states.py
+++ netqasm/util/states.py
@@ -23,7 +23,7 @@ def bloch_sphere_rep(mat):
 
     # Compute polar coordinates
     r = np.linalg.norm([ax, ay, az])
-    assert abs(r - 1) < 0.001, "mat not normalized"
+    assert r < 1.001, "mat not normalized"
     if r == 0:
         return (0, 0, 0)
     theta = np.arccos(az / r)
