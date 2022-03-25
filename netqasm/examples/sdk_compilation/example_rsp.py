1c569ff1bb09ef9d898750c5f1e9e873a5ef468f
Bart van der Vecht <b.vandervecht@tudelft.nl>
2022-03-25 16:16:32 +0100

    Fix example

--- netqasm/examples/sdk_compilation/example_rsp.py
+++ netqasm/examples/sdk_compilation/example_rsp.py
@@ -12,7 +12,7 @@ def main(no_output=False):
     epr_socket = EPRSocket(remote_app_name="Bob")
     with DebugConnection("Alice", epr_sockets=[epr_socket]) as alice:
         m = epr_socket.create_rsp()[0]
-        print(m.measurement_outcome)
+        print(m.raw_measurement_outcome)
 
     if no_output:
         # Third message since the first two are for init application
