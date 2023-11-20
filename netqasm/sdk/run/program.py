9cbd7f6a1b5d2291f33e0abe8a129fc564b53656
Casper <draakie@protonmail.com>
2023-11-20 13:53:46 +0100

    Add parameters property.

--- netqasm/sdk/run/program.py
+++ netqasm/sdk/run/program.py
@@ -3,12 +3,17 @@ External program interface.
 """
 
 from abc import ABC, abstractmethod
+from typing import Dict, Any
 
 from netqasm.sdk.run import Context
 
 class Program(ABC):
     def __init__(self):
         print(__name__)
+
+    @property
+    def parameters(self) -> Dict[str, Any]:
+        raise NotImplementedError
 
     @abstractmethod
     def run(self, context: Context):
