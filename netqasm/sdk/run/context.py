df592929db87db0ca3c9dd4bdd28320281b1e427
Casper <draakie@protonmail.com>
2024-02-12 15:19:51 +0100

    Remove sockets from interface.

--- netqasm/sdk/run/context.py
+++ netqasm/sdk/run/context.py
@@ -3,11 +3,8 @@ External execution context interface.
 """
 
 from abc import ABC, abstractmethod
-from typing import Dict
 
 from netqasm.sdk.connection import BaseNetQASMConnection
-from netqasm.sdk.classical_communication.socket import Socket
-from netqasm.sdk.epr_socket import EPRSocket
 
 class Context(ABC):
     def __init__(self):
@@ -16,14 +13,4 @@ class Context(ABC):
     @property
     @abstractmethod
     def connection(self) -> BaseNetQASMConnection:
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def csockets(self) -> Dict[str, Socket]:
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def epr_sockets(self) -> Dict[str, EPRSocket]:
         raise NotImplementedError
